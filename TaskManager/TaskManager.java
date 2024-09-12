9d6c24b9f944ce383211b2e71d0216bf772b1504
shifterest <86647101+shifterest@users.noreply.github.com>
2024-09-12 10:12:24 +0800

    Create TaskManager.java

---
+++ TaskManager/TaskManager.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TaskManager {
+    private List<Task> tasks;
+
+    // Constructor, getter, and setter
+    public TaskManager () {
+        tasks = new ArrayList<>();
+    }
+    
+    public void addTask (Task task) {
+        tasks.add(task);
+    }
+    
+    // getSizeTaskManager() – a method that returns the size of the 'tasks' ArrayList
+    public int getSizeTaskManager() {
+        return tasks.size();
+    }
+
+    // markTaskAsCompleted() - a method that searches for and marks a task as complete
+    public void markTaskAsCompleted(String description) {
+        for (Task task : tasks) {
+            if (task.getDescription().equals(description)) {
+                task.markAsCompleted();
+                break;
+            }
+        }
+    }
+    
+    // listAllTasks() – a method that displays all tasks from the 'tasks' ArrayList
+    public void listAllTasks() {
+        for (Task task : tasks) {
+            task.displayTask();
+        }
+    }
+    
+    // listAllCompletedTasks() – a method that displays completed tasks from the 'tasks' ArrayList
+    public void listAllCompletedTasks() {
+        int ctr = 0;
+        for (Task task : tasks) {
+            if (task.isCompleted() && !task.getDescription().equals("TaskRemoved")) {
+                task.displayTask();
+                ctr++;
+            }
+        }
+        System.out.println("There are " + ctr + " tasked that has been completed!");
+    }
+    
+    public void listAllNotCompletedTasks() {
+        int ctr = 0;
+        for (Task task : tasks) {
+            if (!task.isCompleted() && !task.getDescription().equals("TaskRemoved")) {
+                task.displayTask();
+                ctr++;
+            }
+        }
+        System.out.println("There are " + ctr + " tasked that has not been completed!");
+    }
+    
+    // removeTask() – a method that changes a specific task's description to "TaskRemoved"
+    public void removeTask(String description) {
+        for (Task task : tasks) {
+            if (task.getDescription().equals(description)) {
+                task.removeTask();
+                break;
+            }
+        }
+    }
+}
